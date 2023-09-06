396ab47e1c0323fccd879f0f47148be4b6d19989
zilaitism <144225905+zilaitism@users.noreply.github.com>
2023-09-06 12:48:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+"use script"
+let a = 10;
+let b = 2;
+alert(a+b);
+alert(a-b);
+alert(a*b);
+alert(a/b);
+let c = 10;
+let d = 5;
+result = c + d;
+alert(result);
+
+
+
+
+
---
+++ script1.js
@@ -0,0 +1,40 @@
+"use script"
+let a1 = 1;
+let b1 = 2;
+let c1 = 3;
+alert(a1+b1+c1);//сложение 
+let a2 = 10;
+let b2 = 5;
+let c2 = a2 - b2;
+let d2 = 7
+let result1 = c2 + d2;
+alert(result1);
+let a3 = 1.5;
+let b3 = 0.75;
+alert(a + b);
+let c3 = -100;/*
+отрицательное число
+*/
+alert (c3);
+let w = 123;
+alert (-w);
+let a4 = 13;
+let b4 = 5;
+alert(a4 % b4);
+let c4 = 2;
+alert(c4 ** 10);
+let name = "Maxim"
+let fam = "Zilaitis"
+alert(name);
+alert(fam);
+str = "!!!";
+alert(str); 
+j = "java";
+s = "script";
+alert(j + s); 
+h = "hello";
+w1 = "world";
+alert(h + " " + w1);
+let str1 = 'ofjojcojkcvaosksqsrdrq';
+alert(str1.length);
+
